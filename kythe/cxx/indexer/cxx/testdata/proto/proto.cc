322af8e47bcaa144a7842113a5e794ca8deded87
Justin Buchanan <justinbuchanan@google.com>
2019-02-13 22:57:05 -0800

    stuff

---
+++ kythe/cxx/indexer/cxx/testdata/proto/proto.cc
@@ -0,0 +1,11 @@
+#include "kythe/cxx/indexer/cxx/testdata/proto/testdata.pb.h"
+
+void test_function() {
+    //- @Message ref CcMessage
+    proto::Message msg;
+    //- @set_string_field ref CcSetStringField
+    msg.set_string_field("blah")
+}
+
+//- Message generates CcMessage
+//- StringField generates CcSetStringField
