7db92eb0a27c649c280cd6efd37ecb09fd1c4e76
Justin Buchanan <justinbuchanan@google.com>
2019-08-07 17:27:57 -0700

    attempting to remove cxx_indexer support from kindex

--- kythe/cxx/indexer/cxx/testdata/test_kindex.sh
+++ kythe/cxx/indexer/cxx/testdata/test_kindex.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 # Tests whether the indexer will read from kindex files.
-set -e
+set -ex
 BASE_DIR="$PWD/kythe/cxx/indexer/cxx/testdata"
 OUT_DIR="$TEST_TMPDIR"
 VERIFIER="kythe/cxx/verifier/verifier"
 INDEXER="kythe/cxx/indexer/cxx/indexer"
 KINDEX_TOOL="kythe/cxx/tools/kindex_tool"
-TEST_INDEX="${OUT_DIR}/test.kindex"
-REPO_TEST_INDEX="${OUT_DIR}/repo_test.kindex"
+KZIP_TOOL="kythe/go/platform/tools/kzip/kzip"
+TEST_KZIP="${OUT_DIR}/test.kzip"
+REPO_TEST_KZIP="${OUT_DIR}/repo_test.kzip"
 mkdir -p "${OUT_DIR}"
-"${KINDEX_TOOL}" -assemble "${TEST_INDEX}" \
-    "${BASE_DIR}/kindex_test.unit" \
-    "${BASE_DIR}/kindex_test.header" \
-    "${BASE_DIR}/kindex_test.main"
-"${INDEXER}" "${TEST_INDEX}" --ignore_unimplemented=false \
+
+echo -e "class Header;\n" > main.h
+echo -e "#include \"main.h\"\n#ifdef CMDARG\nclass Main;\n#else\n#error CMDARG unset--maybe we didn't see the CompilationUnit's arguments?\n#endif\n" > main.cc
+
+
+"${KZIP_TOOL}" create -output "${TEST_KZIP}" \
+    -uri "kythe://kythe?lang=c++#test_kindex" \
+    -working_directory "/" \
+    -source_file "main.cc" \
+    -output_key "main.o" \
+    -argument "unusedexecutable" \
+    -argument "-DCMDARG" \
+    -argument "main.cc" \
+    -required_input "main.h" \
+    -required_input "main.cc" 
+    # "${BASE_DIR}/kindex_test.unit" \
+    # "${BASE_DIR}/kindex_test.header" \
+    # "${BASE_DIR}/kindex_test.main"
+echo "CREATED KZIP: $TEST_KZIP"
+"${INDEXER}" "${TEST_KZIP}" --ignore_unimplemented=false \
     > "${OUT_DIR}/kindex_test.entries"
 cat "${OUT_DIR}/kindex_test.entries" \
-    | "${VERIFIER}" --nocheck_for_singletons --nofile_vnames \
+    | "${VERIFIER}" --nocheck_for_singletons --show_goals --nofile_vnames \
       "${BASE_DIR}/kindex_test.verify"
-# The second test (which is useless unless the first succeeds) checks that
-# we handle relative paths.
-"${KINDEX_TOOL}" -assemble "${REPO_TEST_INDEX}" \
-    "${BASE_DIR}/kindex_repo_test.unit" \
-    "${BASE_DIR}/kindex_repo_test.header" \
-    "${BASE_DIR}/kindex_repo_test.header2" \
-    "${BASE_DIR}/kindex_repo_test.main"
-"${INDEXER}" "${REPO_TEST_INDEX}" --ignore_unimplemented=false \
-    > "${OUT_DIR}/kindex_repo_test.entries"
-cat "${OUT_DIR}/kindex_repo_test.entries" \
-    | "${VERIFIER}" --nocheck_for_singletons --nofile_vnames \
-      "${BASE_DIR}/kindex_repo_test.verify"
-# Finally, check that we can handle Windows paths.
-"${KINDEX_TOOL}" -assemble "${REPO_TEST_INDEX}" \
-    "${BASE_DIR}/windows_test.unit" \
-    "${BASE_DIR}/windows_test.header" \
-    "${BASE_DIR}/windows_test.main"
-"${INDEXER}" "${REPO_TEST_INDEX}" --ignore_unimplemented=false \
-    > "${OUT_DIR}/windows_test.entries"
-cat "${OUT_DIR}/windows_test.entries" \
-    | "${VERIFIER}" --nocheck_for_singletons --nofile_vnames \
-      "${BASE_DIR}/windows_test.verify"
+# # The second test (which is useless unless the first succeeds) checks that
+# # we handle relative paths.
+# "${KINDEX_TOOL}" -assemble "${REPO_TEST_KZIP}" \
+#     "${BASE_DIR}/kindex_repo_test.unit" \
+#     "${BASE_DIR}/kindex_repo_test.header" \
+#     "${BASE_DIR}/kindex_repo_test.header2" \
+#     "${BASE_DIR}/kindex_repo_test.main"
+# "${INDEXER}" "${REPO_TEST_KZIP}" --ignore_unimplemented=false \
+#     > "${OUT_DIR}/kindex_repo_test.entries"
+# cat "${OUT_DIR}/kindex_repo_test.entries" \
+#     | "${VERIFIER}" --nocheck_for_singletons --nofile_vnames \
+#       "${BASE_DIR}/kindex_repo_test.verify"
+# # Finally, check that we can handle Windows paths.
+# "${KINDEX_TOOL}" -assemble "${REPO_TEST_KZIP}" \
+#     "${BASE_DIR}/windows_test.unit" \
+#     "${BASE_DIR}/windows_test.header" \
+#     "${BASE_DIR}/windows_test.main"
+# "${INDEXER}" "${REPO_TEST_KZIP}" --ignore_unimplemented=false \
+#     > "${OUT_DIR}/windows_test.entries"
+# cat "${OUT_DIR}/windows_test.entries" \
+#     | "${VERIFIER}" --nocheck_for_singletons --nofile_vnames \
+#       "${BASE_DIR}/windows_test.verify"
 
 
-# TODO(justbuchanan): delete or convert to kzip
+# # TODO(justbuchanan): delete or convert to kzip
